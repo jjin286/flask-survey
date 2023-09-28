9b0b170570a0848154bfa0b330685dc46b133643
JaeWon Jin <j1jin286@gmail.com>
2023-09-28 15:29:31 -0700

    Completed to step 9

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 @app.get("/")
 def home():
     """Returns the start survey page"""
+
+    session["responses"] = []
 
     return render_template(
         "survey_start.html",
@@ -23,8 +23,6 @@ def home():
 def begin():
     """Redirects to start survey"""
 
-    responses.clear() #Clear everytime we start a new survey
-
     return redirect("/questions/0")
 
 
@@ -32,9 +30,21 @@ def begin():
 def show_question(question_num):
     """Returns question number question_num"""
 
-    return render_template(
-        "question.html",
-        question=survey.questions[question_num])
+    num_of_responses = len(session["responses"])
+
+    if num_of_responses == question_num:
+        return render_template(
+            "question.html",
+            question=survey.questions[question_num])
+
+    elif num_of_responses == len(survey.questions):
+        flash("You've already completed the survey!")
+        return redirect("/completion")
+
+    else:
+        flash(f"You haven't answered question { num_of_responses }")
+        return redirect(f"/questions/{ num_of_responses }")
+
 
 
 @app.post("/answer")
@@ -42,12 +52,15 @@ def handle_answer():
     """Store answer locally and redirect to next question"""
 
     answer = request.form.get('answer')
-    curr_question_index = len(responses)
+    curr_question_index = len(session['responses'])
     survey_question_count = len(survey.questions)
 
     if(answer):
+        responses = session['responses']
         responses.append(answer)
-        curr_question_index = len(responses)
+        session['responses'] = responses
+
+        curr_question_index = len(session['responses'])
 
         if(curr_question_index == survey_question_count):
             return redirect("/completion")
@@ -55,22 +68,19 @@ def handle_answer():
         return redirect(f"/questions/{curr_question_index}")
 
     else:
-        error = "Please select a choice before continuing"
-        return render_template(
-        "question.html",
-        question=survey.questions[curr_question_index],
-        error=error)
+        flash("Please select a choice before continuing")
+
+        return redirect(f"/questions/{curr_question_index}")
 
 
 @app.get("/completion")
 def complete():
     """Return completed survey page"""
 
-    survey_question_count = len(responses)
+    survey_question_count = len(session['responses'])
 
     return render_template(
                 "completion.html",
                 question_count=survey_question_count,
-                complete_responses=responses,
                 questions=survey.questions)
 
